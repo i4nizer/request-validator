f4b6ddaa7bc9d0ef9bca52d759f2eac2f4bdb0ce
i4nizer <iansandoval264@gmail.com>
2024-11-05 20:21:54 +0800

    added types

---
+++ index.d.ts
@@ -0,0 +1,24 @@
+declare module './request-validator' {
+    interface Field {
+        name: string;
+        min?: number;
+        max?: number;
+        pattern?: RegExp;
+        required?: boolean;
+    }
+
+    interface RequestValidator {
+        getMissingFields: (fields: Field[], obj: object) => string[];
+        validateStringField: (field: Field, value: string) => string | null;
+        validateNumberField: (field: Field, value: number) => string | null;
+        validateField: (field: Field, value: any) => string | null;
+        validateRequest: (
+            fields: Field[],
+            obj: object,
+            res: { status: (code: number) => { send: (message: string) => void } }
+        ) => boolean;
+    }
+
+    const requestValidator: RequestValidator;
+    export = requestValidator;
+}
